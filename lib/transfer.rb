f4f2e7ea503c5a299a61821183bbb5a8dd083bfd
Raveen K Sharma <raveen08@gmail.com>
2020-07-31 22:50:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
     
   
   def execute_transaction
-    if valid? && sender.balance > amount && self.status = "pending"
+    if valid? && sender.balance > amount && self.status == "pending"
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
