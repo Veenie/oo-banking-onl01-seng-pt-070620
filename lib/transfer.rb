d52b51f9a6b3a44be2d85fb2af31f172fbf89834
Raveen K Sharma <raveen08@gmail.com>
2020-07-31 22:26:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,12 @@
 class Transfer
   # your code here
-  
+  attr_accessor :name, :reciever, :status, :amount
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "pending"
+  end
   
   
 end
